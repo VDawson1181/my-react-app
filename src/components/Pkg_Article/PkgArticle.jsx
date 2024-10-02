136497757305ae0c1ec9f1412ab86be11d199f11
Val Dawson <vdawson@pch.com>
2024-10-02 18:52:24 -0400

    packages added

--- src/components/Pkg_Article/PkgArticle.jsx
+++ src/components/Pkg_Article/PkgArticle.jsx
@@ -5,34 +5,42 @@ const PkgArticle = (props) => {
 const pkgId = props.id;
 const userList = props.items;
 const imageList = props.images;
-    
-    const cards = userList.map((item, index) => <aside key={index}>
-                <h1>{item.name}</h1>
-                <h2>Objective:</h2>
-                <p className='pkgObj'>{item.objective}</p>                
-                <h2>Inspiration:</h2>
-                <p className='pkgInspo'>{item.inspiration}</p>  
-                <p className='pkgResult'>{item.result}</p>              
-                </aside>);
+
 
     const images = imageList.map( (image, index) => <div key={index}>
-            <img key={index} className="card-image" src={ image.url } alt="Image" loading="lazy"/>
-            <p>{image.blurb}</p>
-        </div>);
+                <img className="card-image" src={ image.url } alt="Image" loading="lazy"/>
+                <p>{image.blurb}</p>
+                </div>);
     
-  return (
-    <div id={pkgId} className='pkgArticleMain'>
+    const packages = userList.map((item, index) => <div key={pkgId} className='pkgArticleMain'>
+        <h2 className='clicker'>{item.title}</h2>
         <div className="pkgArticle_PackageDescription">
-            {cards} 
+                <aside>
+                    <h1>"{item.title}" - <span>{item.type}</span></h1>
+                    <h2>Objective:</h2>
+                    <p className='pkgObj'>{item.objective}</p>                
+                    <h2>Inspiration:</h2>
+                    <p className='pkgInspo'>{item.inspiration}</p>  
+                    <p className='pkgResult'>{item.result}</p>              
+                </aside>
         </div>
+
         <div className="pkgArticle_PackageRundown">
             <ul>
                 <li>
                     {images}
                 </li>
             </ul>
-        </div>        
+        </div> 
     </div>
+    );
+
+
+    
+  return (<>
+  {packages} 
+  </>
+
   )
 }
 
--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -7,7 +7,8 @@ const Portfolio = () => {
   const gt_users = [
     {
       id: "goodThings",
-      name: "\"Good Things Are Headed Your Way\"- Ecommerce Direct Marketing Package", 
+      title: "Good Things Are Headed Your Way",
+      type: "Ecommerce Direct Marketing Package",
       objective: "This is part of a new email series that I pitched to the marketing team to help generate incremental revenue for the company.  It performs very well, bringing in approximately $1.2 Million in unplanned revenue annually. The overall idea was to thank recent orderers for their patronage by “surprising and delighting” them with an exciting savings opportunity that no other customers were getting. We currently have 5 other creatives rotating in the series.",
       inspiration: "The idea for this version: Show a modern, animated email that gives a bold statement regarding this recent order and teases a special reward incentive that's waiting for them on the landing page. It's an interactive balloon pop experience on the LP that reveals the discount.", 
       result:"",
@@ -24,7 +25,8 @@ const Portfolio = () => {
   const mt_users = [
     {
       id: "moneyTree",
-      name: "\"Money Tree Interactive Game\"- Ecommerce Direct Marketing Package", 
+      title: "Money Tree",
+      type: "Ecommerce Direct Marketing Package",
       objective: "Create an interactive game that would surprise and delight users with both an exciting opportunity to win and a special “free gift” order incentive.",
       inspiration: "The idea for this version: Ever wish you had your own personal money tree? Yeah, me too. That's what gave me the idea here. The user gets presented with the sweepstakes section first. They click to shake the tree and cash falls down and tallies up. The prize was made variable, ranging from $50,000-$100,000. Playing off of the excitement of the sweepstakes, we haloed the theme for the product section, giving the user a chance to shake the tree for a free gift order incentive. Users received either a $25 or $30 free gift value with their order based on their buyer segment with the most loyal customers revealing the highest amount. Design-wise, we kept it light and playful. We added a little birdie who “chirped” and relayed important UI messaging to the user. We even had elements like a boot falling out of one of the trees as a fun add-on.",
       result:"This performed very well. It tested at a +18 Lift to Order Response.", 
@@ -44,7 +46,8 @@ const Portfolio = () => {
   const pi_users = [
     {
       id: "positiveImpact",
-      name: "\"Positive Impact\"- Ecommerce Direct Marketing Package", 
+      title: "Positive Impact",
+      type: "Ecommerce Direct Marketing Package",
       objective: "Create a new contest promotion that is uplifting to the users and provides a substantial payout for our imminent prize event",
       inspiration: "This package was written in the thick of Covid lockdown. A lot of people around the country were feeling the financial strains of furloughs and budget cuts. But at the same time, there was a strong sense of community across the country. People were (virtually) checking in on family, friends and neighbors and offering to help anyway they could. It gave me the idea to create an Americana-themed package with a strong sweepstakes offer that would give substantial financial relief and impact the winner’s life in a positive way. This “positive impact” would help wipe out debt, cushion savings, even help out loved ones. Design-wise, we chose a red, white and blue color pallet with elements from our national landmarks. We even included a virtual apple pie recipe card that users could download. (I wrote and tested the recipe myself)!",
       result:"This performed very well. It tested at a +18 Lift to Order Response.", 
@@ -65,36 +68,53 @@ const Portfolio = () => {
   const uc_users = [
     {
       id: "uncutChecks",
-      name: "\"UNCUT CHECKS\"- Ecommerce Direct Marketing Package", 
+      title: "Uncut Checks",
+      type: "Ecommerce Direct Marketing Package",
       objective: "Create a new daily package that can be used with the promotional TV timeframe.",
       inspiration: "Come up with a fresh approach to showcase the Win For Life prize being offered. The lifetime payments are paid out weekly, which comes out to a lot of checks being printed. That inspired the idea of freshly printed uncut money sheets. Here we switched out the cash for faux-check sheets. Design-wise, we wanted to go big and bold with a modern, clean letter.",
-      result:"This performed very well. It tested at a +18 Lift to Order Response.", 
+      result:"This package performed stellar. It was a +19 in unique clicks, + 16 in lift to order response", 
       isValid: true, 
     },
   ];
   const uc_images = [
-    {url: "./src/assets/packages/positiveImpact/PI_Email_Top.png", blurb: ""},
-    {url: "./src/assets/packages/positiveImpact/PI_Email_Mid.gif", blurb: ""},
-    {url: "./src/assets/packages/positiveImpact/PI_Email_Body.png", blurb: "The upfront message teases THREE positive impact checks with the last check remaining a mystery until the user clicks through to the landing page."},
-    {url: "./src/assets/packages/positiveImpact/PI_Pkg_1.png", blurb: "On load of the landing page, the user gets a pop-up message asking him to make a decision regarding how much the last positive impact check should be made out for if he wins from this notice. Johnny is choosing between three prize values: Cash for a Vacation, Cash for a new Car, or Cash towards a Charity."},
-    {url: "./src/assets/packages/positiveImpact/PI_Pkg_2.png", blurb: "Once Johnny picks, he is asked to confirm his choice."},
-    {url: "./src/assets/packages/positiveImpact/PI_Pkg_3.png", blurb: ""},
-    {url: "./src/assets/packages/positiveImpact/PI_Pkg_4.png", blurb: "This was placed into an affinity mailing targeted to buyers who have shown an interest in kitchen items. We added a fun virtual recipe card that Johnny could print for free."},
+    {url: "./src/assets/packages/uncutChecks/UC_Email.png", blurb: "The top of the email is attention-grabbing with an oversized prize statement overlapping the checks to emphasize the massive payout amount. We used minimal copy above the fold as a teaser and kept the letter informational and on the shorter side."},
+    {url: "./src/assets/packages/uncutChecks/UC_1.png", blurb: "Landing Page: The user clicks through the email and sees a continuation of the uncut checks theme."},
+    {url: "./src/assets/packages/uncutChecks/UC_2.png", blurb: "Call to order: We segmented the messaging here, targeting our lower tier of orderers, encouraging them to place a new order today to “level up” and start receiving bigger and better customer level benefits."},
+    {url: "./src/assets/packages/uncutChecks/UC_3.png", blurb: ""},
+    {url: "./src/assets/packages/uncutChecks/UC_4.png", blurb: "Supporting Assets: These assets are sprinkled throughout the lineup between products on tab 2 and 3, as a reminder to the user that their entry has not yet been submitted and they have to keep going to complete it."},
+    {url: "./src/assets/packages/uncutChecks/UC_5.png", blurb: "Entry Confirmation Stamp"},
+  ];
+
+  // We're Getting Close
+  const wgc_users = [
+    {
+      id: "weGettinClose",
+      title: "We're Getting Close",
+      type: "Ecommerce Direct Marketing Package", 
+      objective: "Create a new email package that can be used during the final 10 days to enter countdown timeframe.",
+      inspiration: "This was based around my personal experience, while making a prize delivery on behalf of PCH. I recalled the electricity of anticipation as we drove down the highway, closing the miles between our hotel and the winner’s location, inching in excitement with each highway sign we passed. That inspired me to create a fun package that puts Johnny in the Prize Patrol van, dreaming about his own winning moment. Will we be coming for him next? Will it be his we’re closing in on in just days to award our next big prize?  We added personalization throughout including on the highway signs to help reinforce that it could very well be Johnny’s home we visit next if he enters and wins. Design-wise, we wanted to keep this airy and playful to give the entrant room to dream about the route we could be taking to their home.",
+      result:"This package was a very high performer. Due to its success, we created a V2 of the package.", 
+      isValid: true, 
+    },
+  ];
+  const wgc_images = [
+    {url: "./src/assets/packages/wereGettingClose/wgc_Email.png", blurb: "E-Mail"},
+    {url: "./src/assets/packages/wereGettingClose/wgc_1.png", blurb: "Landing Page: We used a driving animation here to help set the scene of a prize delivery. It's a dream sequence that outs the user in the driver's seat. This animation autostarts on load and continues for 4 stages until a hard with the call to action for Johnny to enter is presented."},
+    {url: "./src/assets/packages/wereGettingClose/wgc_2.png", blurb: "We designed a custom billboard"},
+    {url: "./src/assets/packages/wereGettingClose/wgc_3.png", blurb: ""},
+    {url: "./src/assets/packages/wereGettingClose/wgc_4.png", blurb: "Balloons swaying in the breeze"},
+    {url: "./src/assets/packages/wereGettingClose/wgc_5.png", blurb: "We continued the driving theme for the product section. Here we created a digital sign that switched out messaging in two stages."},
+    {url: "./src/assets/packages/wereGettingClose/wgc_6.png", blurb: "Included targeted messaging for lapsed buyers"},
   ];
 
   return (
     <>
-      <h2>Portfolio</h2>
-      <ul className="portfolioList">
-        <li>Good Things</li>
-        <li>Money Tree</li>
-        <li>Positive Impact</li>
-        <li>Uncut Checks</li>
-        <li>We're Getting Close</li>
-      </ul>
+      <h1>Portfolio</h1>
       <PkgArticle id="goodThings" items={gt_users} images={gt_images}/>
-      <PkgArticle id="MoneyTree" items={mt_users} images={mt_images}/>
-      <PkgArticle id="PositiveImpact" items={pi_users} images={pi_images}/>
+      <PkgArticle id="moneyTree" items={mt_users} images={mt_images}/>
+      <PkgArticle id="positiveImpact" items={pi_users} images={pi_images}/>
+      <PkgArticle id="uncutChecks" items={uc_users} images={uc_images}/>
+      <PkgArticle id="weGettinClose" items={wgc_users} images={wgc_images}/>
     </>
   )
 }
