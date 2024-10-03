e944940a2cfdcd41812e907fe3362857b9abfe5f
Val Dawson <vdawson@pch.com>
2024-10-03 02:30:28 -0400

    Took out use ref example from homepage...

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Card from '../../src/Card.jsx'
 import DigitalClockApp from "../components/DigitalClock/DigitalClockApp.jsx"
-import UseRefComponent from '../useRefExample/UseRefComponent.jsx'
 
 
 const Home = () => {
@@ -10,7 +9,6 @@ const Home = () => {
         <h2>Homepage</h2>
         <DigitalClockApp type={1}></DigitalClockApp>
         <DigitalClockApp type={2}></DigitalClockApp>
-        {/* <UseRefComponent></UseRefComponent> */}
     </>
   )
 }
